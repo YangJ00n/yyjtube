24d80c7eecac41fcb1b5e3600d5e7f964cd2579f
YangJ00n <yangcoding820@gmail.com>
2021-12-26 21:23:16 +0900

    Use Morgan

--- src/server.js
+++ src/server.js
@@ -1,20 +1,19 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 // create express application
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+const logger = morgan("dev"); // morgan return middleware
+app.use(logger);
 
 const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-app.get("/", logger, handleHome);
+app.get("/", handleHome);
 
 // listening
 const handleListening = () =>
