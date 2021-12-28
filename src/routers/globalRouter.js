db9be2bbc65746869047068eef216952bcf8f01a
YangJ00n <yangcoding820@gmail.com>
2021-12-28 23:20:10 +0900

    POST requests
    Make 'Watch video page' and 'Edit video page' using Fake Database

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { trending, search } from "../controllers/videoController";
+import { trending } from "../controllers/videoController";
 
 const globerRouter = express.Router();
 
 globerRouter.get("/", trending);
 globerRouter.get("/join", join);
 globerRouter.get("/login", login);
-globerRouter.get("/search", search);
 
 export default globerRouter;
--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ const logger = morgan("dev"); // morgan return middleware
 app.set("view engine", "pug"); // set view engine as pug
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true })); // form의 value들을 이해할 수 있도록 하고, JS 형식으로 변형시켜준다.
 app.use("/", globerRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
