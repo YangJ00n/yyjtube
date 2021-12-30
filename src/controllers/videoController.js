a6a787f0619d0bfaf132848552690fa380ab65e7
YangJ00n <yangcoding820@gmail.com>
2021-12-30 23:03:42 +0900

    More Practice
    Make 'Upload video page' using Fake Database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,11 +36,29 @@ export const watch = (req, res) => {
 export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+
+  return res.redirect("/");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { watch, getEdit, postEdit } from "../controllers/videoController";
+import {
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
