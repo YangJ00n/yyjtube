72c4583503cc2341bc64fc26e2c28ee2220151de
YangJ00n <yangcoding820@gmail.com>
2022-01-18 15:43:15 +0900

    Creating a Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,9 +4,10 @@ export const home = async (req, res) => {
   const videos = await Video.find({}); // database에서 결과값을 받을 때까지 js는 이 라인에서 계속 기다림 -> 순서대로 실행됨
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  return res.render("watch", { pageTitle: video.title, video });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
@@ -21,7 +22,19 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hastags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hastags: hastags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
-  hastags: [{ type: String }],
+  title: { type: String, required: true, trim: true, maxlength: 80 },
+  description: { type: String, required: true, trim: true, minlength: 20 },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hastags: [{ type: String, trim: true }],
   meta: {
-    view: Number,
-    rating: Number,
+    view: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
   },
 });
 
