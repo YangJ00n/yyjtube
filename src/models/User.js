6345bc0e4f44199989bb67a93782dbebe3cf419e
YangJ00n <yangcoding820@gmail.com>
2022-01-24 01:45:28 +0900

    Github Login

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
   name: { type: String, required: true },
   location: String,
 });
