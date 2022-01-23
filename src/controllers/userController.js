789b06e30173d6c4008c453d41d8d373a676780d
YangJ00n <yangcoding820@gmail.com>
2022-01-24 01:53:07 +0900

    Log Out

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -137,7 +137,10 @@ export const finishGithubLogin = async (req, res) => {
   }
 };
 
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
+
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
