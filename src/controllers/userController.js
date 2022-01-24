99e6a19417f1db45be167f0bedad091bcda442cf
YangJ00n <yangcoding820@gmail.com>
2022-01-24 14:50:52 +0900

    Protector and Public Middlewares
    + Edit Profile GET

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -142,5 +142,10 @@ export const logout = (req, res) => {
   return res.redirect("/");
 };
 
-export const edit = (req, res) => res.send("Edit User");
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+  return res.render("edit-profile");
+};
 export const see = (req, res) => res.send("See User");
--- src/middlewares.js
+++ src/middlewares.js
@@ -2,6 +2,24 @@ export const localsMiddleware = (req, res, next) => {
   // pug파일에서 res.locas에 접근할 수 있다.
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
   next();
+};
+
+// 로그인 되어 있으면 통과하는 middleware
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+// 로그인 되어 있지 않으면 통과하는 middleware
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
