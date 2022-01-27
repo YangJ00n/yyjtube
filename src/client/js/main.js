0c157469ed1f3c523b34a453c98ff0f6a71146aa
YangJ00n <yangjoon820@gmail.com>
2022-01-27 14:28:18 +0900

    Webpack Configuration - SCSS
    + Better Developer Experience and Edit Scripts

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,1 +1,3 @@
-alert("hi");
+import "../scss/styles.scss";
+
+console.log("hi");
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
+  watch: true, // 파일을 감시(watch)하고 변경될 때마다 다시 build 한다.
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true, // output folder를 bulid 시작 전에 clean 해준다.
   },
   module: {
     rules: [
@@ -17,6 +25,10 @@ module.exports = {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
