4784d45cf830c45893966a9284e7aeddfb53135b
YangJ00n <yangjoon820@gmail.com>
2022-02-02 16:29:43 +0900

    Recording Video and Downloading the File

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,14 +1,55 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    // console.log(event.data);
+    // createObjectURL : 파일이 있는 브라우저의 메모리를 가리키고 있는 URL을 생성한다.
+    videoFile = URL.createObjectURL(event.data);
+    // console.log(videoFile);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
     video: true,
   });
-  console.log(stream);
+  //   console.log(stream);
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
